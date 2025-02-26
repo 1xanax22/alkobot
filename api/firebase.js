46e12d1c394982ff0dad6eb76845ccaabd59c1aa
1xanax22 <75693562+1xanax22@users.noreply.github.com>
2025-02-26 17:06:12 +0300

    Update firebase.js

--- api/firebase.js
+++ api/firebase.js
@@ -1,1 +1,16 @@
-
+export default function handler(req, res) {
+    const firebaseConfig = {
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        databaseURL: process.env.FIREBASE_DATABASE_URL,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
+        measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    };
+    res.status(200).json({
+        firebaseConfig: firebaseConfig,
+        botToken: process.env.BOT_TOKEN
+    });
+}
